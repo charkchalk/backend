454910877180a0b368da5d39dcb327d7c1b3d054
mochizuki <1p41p4jejo@gmail.com>
2022-10-25 00:50:04 +0800

    chore: add open API spec to bean

---
+++ src/main/java/io/charkchalk/backend/configuration/OpenAPIConfig.java
@@ -0,0 +1,28 @@
+package io.charkchalk.backend.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI openAPIConfig() {
+        return new OpenAPI()
+                .info(new Info().title("Charkchalk Backend API")
+                        .description("Charkchalk Backend API implemented with Spring Boot RESTful service and documented using springdoc-openapi and OpenAPI 3.")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0.txt")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Charkchalk Database Documentation")
+                        .url("https://dbdocs.io/YukinaMochizuki/Charkchalk"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+    }
+}
