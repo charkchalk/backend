f1242466db607592b521abfe08f9a434856e212e
mochizuki <1p41p4jejo@gmail.com>
2022-10-26 02:38:30 +0800

    docs: remove the unused license

--- src/main/java/io/charkchalk/backend/configuration/OpenAPIConfig.java
+++ src/main/java/io/charkchalk/backend/configuration/OpenAPIConfig.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,9 +16,7 @@ public class OpenAPIConfig {
                 .info(new Info().title("Charkchalk Backend API")
                         .description("Charkchalk Backend API implemented with Spring Boot RESTful service " +
                                 "and documented using springdoc-openapi and OpenAPI 3.")
-                        .version("v0.0.1")
-                        .license(new License().name("Apache 2.0")
-                                .url("https://www.apache.org/licenses/LICENSE-2.0.txt")))
+                        .version("v0.0.1"))
                 .externalDocs(new ExternalDocumentation()
                         .description("Charkchalk Database Documentation")
                         .url("https://dbdocs.io/YukinaMochizuki/Charkchalk"))
