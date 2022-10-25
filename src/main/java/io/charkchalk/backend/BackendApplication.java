0a59a347a23902a65bf1126f9bc80b30e46c3a59
mochizuki <1p41p4jejo@gmail.com>
2022-10-26 02:37:13 +0800

    style: fix coding style issues

--- src/main/java/io/charkchalk/backend/BackendApplication.java
+++ src/main/java/io/charkchalk/backend/BackendApplication.java
@@ -8,6 +8,8 @@ import org.springframework.shell.standard.ShellComponent;
 @ShellComponent
 public class BackendApplication {
 
+    private BackendApplication() {}
+
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
