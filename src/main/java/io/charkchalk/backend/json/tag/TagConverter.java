443a8a9b97ab51f445bde97389d8edb344655b78
mochizuki <1p41p4jejo@gmail.com>
2022-11-20 03:07:44 +0800

    feat: add support for pageable in TagConverter

--- src/main/java/io/charkchalk/backend/json/tag/TagConverter.java
+++ src/main/java/io/charkchalk/backend/json/tag/TagConverter.java
@@ -3,13 +3,19 @@ package io.charkchalk.backend.json.tag;
 import io.charkchalk.backend.entity.Tag;
 import io.charkchalk.backend.exception.FieldNotValidItem;
 import io.charkchalk.backend.json.JsonConverter;
+import io.charkchalk.backend.json.PageJson;
 import io.charkchalk.backend.repository.TagRepository;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Component
 public class TagConverter {
@@ -42,9 +48,43 @@ public class TagConverter {
         return tagJson;
     }
 
-    public void updateEntity(@NotNull Tag tag, @NotNull TagJson tagJson) {
-        tag.setName(tagJson.getName());
-        tag.setDescription(tagJson.getDescription());
-        tag.setTagLimit(tagJson.getTagLimit());
+    public PageJson<TagJson> convertToJsonPage(Page<Tag> tags) {
+        PageJson<TagJson> pageJson = new PageJson<>();
+        pageJson.setTotalPages(tags.getTotalPages());
+        pageJson.setCurrentPage(tags.getNumber());
+
+        if(!tags.isEmpty()){
+            for(Tag tag : tags.getContent()) {
+                pageJson.getContent().add(convertToJson(tag));
+            }
+        }
+
+        return pageJson;
+    }
+
+    public void updateEntity(@NotNull Tag tag, @NotNull BaseTagJson baseTagJson) {
+        tag.setName(baseTagJson.getName());
+        tag.setDescription(baseTagJson.getDescription());
+        tag.setTagLimit(baseTagJson.getTagLimit());
+    }
+
+    public static void checkPageable(Pageable pageable) {
+        List<FieldNotValidItem> fieldNotValidItems = new ArrayList<>();
+
+        if(pageable.getSort().isSorted()) {
+            Sort sort = pageable.getSort();
+
+            Set<String> possibleProperty = new HashSet<>();
+            possibleProperty.add("name");
+            possibleProperty.add("tagLimit");
+
+            for(Sort.Order order: sort) {
+                if(!possibleProperty.contains(order.getProperty())) {
+                    fieldNotValidItems.add(FieldNotValidItem.sortPropertyNotFound(order.getProperty(), "Tag"));
+                }
+            }
+        }
+
+        JsonConverter.checkFieldNotValidException(fieldNotValidItems);
     }
 }
