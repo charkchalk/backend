cbf8d327dd650c041ef4798cf3473ff1ae8f00a2
mochizuki <1p41p4jejo@gmail.com>
2022-11-26 00:57:38 +0800

    feat: add range crud controller

---
+++ src/main/java/io/charkchalk/backend/service/range/date/DateController.java
@@ -0,0 +1,69 @@
+package io.charkchalk.backend.service.range.date;
+
+import io.charkchalk.backend.entity.range.DateRange;
+import io.charkchalk.backend.json.PageJson;
+import io.charkchalk.backend.json.range.date.BaseDateRangeJson;
+import io.charkchalk.backend.json.range.date.DateRangeConverter;
+import io.charkchalk.backend.json.range.date.DateRangeJson;
+import io.charkchalk.backend.repository.DateRangeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+
+@RestController
+public class DateController {
+
+    @Autowired
+    private DateRangeConverter dateRangeConverter;
+
+    @Autowired
+    private DateRangeRepository dateRangeRepository;
+
+    @PostMapping("/api/range/date")
+    public ResponseEntity<DateRangeJson> createDateRange(@Valid @RequestBody BaseDateRangeJson baseDateRangeJson) {
+        DateRange dateRange = dateRangeConverter.convertToEntity(baseDateRangeJson);
+        dateRangeRepository.save(dateRange);
+        return ResponseEntity.ok(dateRangeConverter.convertToJson(dateRange));
+    }
+
+    @GetMapping("/api/range/date")
+    public ResponseEntity<PageJson<DateRangeJson>> listDateRanges(Pageable pageable) {
+        DateRangeConverter.checkPageable(pageable);
+        return ResponseEntity.ok(dateRangeConverter.convertToJsonPage(dateRangeRepository.findAll(pageable)));
+    }
+
+    @GetMapping("/api/range/date/{id}")
+    public ResponseEntity<DateRangeJson> getDateRange(@PathVariable @NotNull Long id) {
+        Optional<DateRange> dateRangeOptional = dateRangeRepository.findById(id);
+        return dateRangeOptional.map(value -> ResponseEntity.ok(dateRangeConverter.convertToJson(value)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/api/range/date/{id}")
+    public ResponseEntity<DateRangeJson> putDateRange(@PathVariable @NotNull Long id, @Valid @RequestBody BaseDateRangeJson baseDateRangeJson) {
+        Optional<DateRange> dateRangeOptional = dateRangeRepository.findById(id);
+        if (dateRangeOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        DateRange dateRange = dateRangeOptional.get();
+        dateRangeConverter.updateEntity(dateRange, baseDateRangeJson);
+        dateRangeRepository.save(dateRange);
+        return ResponseEntity.ok(dateRangeConverter.convertToJson(dateRange));
+    }
+
+    @DeleteMapping("/api/range/date/{id}")
+    public ResponseEntity<Void> deleteDateRange(@PathVariable @NotNull Long id) {
+        Optional<DateRange> dateRangeOptional = dateRangeRepository.findById(id);
+        if (dateRangeOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        dateRangeRepository.delete(dateRangeOptional.get());
+        return ResponseEntity.ok().build();
+    }
+}
