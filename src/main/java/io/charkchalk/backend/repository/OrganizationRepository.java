cf486dc1d09f026d3dbf0fb6d9f68fdb032261d9
mochizuki <1p41p4jejo@gmail.com>
2023-02-24 23:29:33 +0800

    feat: add organization converter

--- src/main/java/io/charkchalk/backend/repository/OrganizationRepository.java
+++ src/main/java/io/charkchalk/backend/repository/OrganizationRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface OrganizationRepository extends PagingAndSortingRepository<Organization, Long> {
     Optional<Organization> findByName(String name);
+    boolean existsByName(String name);
 }
