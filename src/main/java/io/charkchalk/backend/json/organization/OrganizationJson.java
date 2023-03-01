c223edc6611d031378cf3898c649ca05d9e76b25
mochizuki <1p41p4jejo@gmail.com>
2023-03-01 21:39:49 +0800

    feat: add uuid and parent to organization json model

--- src/main/java/io/charkchalk/backend/json/organization/OrganizationJson.java
+++ src/main/java/io/charkchalk/backend/json/organization/OrganizationJson.java
@@ -1,23 +1,12 @@
 package io.charkchalk.backend.json.organization;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
-import java.util.Collection;
+import java.util.UUID;
 
 @Getter
 @Setter
-public class OrganizationJson {
-    @NotBlank
-    @JsonProperty("name")
-    private String name;
-
-    @NotBlank
-    @JsonProperty("description")
-    private String description;
-
-    @JsonProperty("tags")
-    private Collection<String> tags;
+public class OrganizationJson extends BaseOrganizationJson{
+    private UUID uuid;
 }
