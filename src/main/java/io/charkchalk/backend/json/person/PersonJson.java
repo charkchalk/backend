f32deba56b213c0e89753ddaa6b3236e88c84296
mochizuki <1p41p4jejo@gmail.com>
2023-04-03 03:27:00 +0800

    style: rename person uuid field

--- src/main/java/io/charkchalk/backend/json/person/PersonJson.java
+++ src/main/java/io/charkchalk/backend/json/person/PersonJson.java
@@ -1,16 +1,12 @@
 package io.charkchalk.backend.json.person;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.Min;
 import java.util.UUID;
 
 @Getter
 @Setter
 public class PersonJson extends BasePersonJson {
-    @Min(1)
-    @JsonProperty("id")
     private UUID uuid;
 }
