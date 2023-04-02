f2baaacc7078db7060396f9b277a2e1e5e58b3c9
mochizuki <1p41p4jejo@gmail.com>
2023-04-03 03:26:55 +0800

    feat: add find by all field in time range repository

--- src/main/java/io/charkchalk/backend/repository/TimeRangeRepository.java
+++ src/main/java/io/charkchalk/backend/repository/TimeRangeRepository.java
@@ -5,7 +5,10 @@ import io.charkchalk.backend.entity.range.TimeRange;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.time.LocalTime;
+import java.util.Optional;
 
 public interface TimeRangeRepository extends PagingAndSortingRepository<TimeRange, Long> {
+    Optional<TimeRange> findByStartTimeAndEndTimeAndWeekday(LocalTime startTime, LocalTime endTime, Weekday weekday);
+
     boolean existsByStartTimeAndEndTimeAndWeekday(LocalTime startTime, LocalTime endTime, Weekday weekday);
 }
